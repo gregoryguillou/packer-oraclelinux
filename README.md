08fadd396ad7c7d6ecc5dd84d05b644cf07953d0
Gregory Guillou <gregory@blue.resetlogs.com>
2016-09-25 23:02:31 +0200

    Simple VM creation

--- README.md
+++ README.md
@@ -1,2 +1,33 @@
 # packer-oraclelinux
-Building Oracle Linux with Packer/Virtualbox
+
+This packages demonstrates how to create an Oracle Linux OVA with Packer/Virtualbox to use with AWS EC2. It can easily be adapted to create any kind of images for Cloud Providers or VMWare/OracleVM
+
+## Configuration
+
+A few pre-requisites must be met in order for that project to work as expected. Among them:
+- Python3 must be installed and in the PATH
+- Packer must be installed 
+- Virtualbox must be installer
+- You must download the Oracle Linux 7.2 ISO file
+
+the ```project.properties```. It must contain the 4 values below:
+
+```
+PROJECT_HOME=/home/gregory/project/packer-oraclelinux
+PACKER=/home/gregory/distribs/packer
+OS_DISTRIBUTION=/home/gregory/distribs/V100082-01.iso
+REPO_URL=http://10.0.2.2/repo
+```
+
+## Building a VM
+
+```vm-generate.py``` allows to create a VM. Use the 2 parameters below:
+- ```-t vbox-ol72``` defines the template name
+- ```-n``` allows to name the VM
+
+The command below shows how to use the script:
+
+```
+./vm-generate.py -t vbox-ol72 -n purple
+```
+
---
+++ scripts/ol7/init-ol.sh
@@ -0,0 +1,46 @@
+#!/bin/bash
+#
+# Update RPMs with the latest release
+#
+# sudo -n yum -y update
+#
+# Clean the configuration and switch to RHCK before you upload the
+# OVF file to AWS...
+#
+
+# Set network to eth0 and remove links to HWADDR
+sed -i '/HWADDR/d' /etc/sysconfig/network-scripts/ifcfg-eth0
+sed -i '/UUID/d' /etc/sysconfig/network-scripts/ifcfg-eth0
+cat /etc/sysconfig/network-scripts/ifcfg-eth0
+
+# Update RPMs
+cd /etc/yum.repos.d
+curl -O http://public-yum.oracle.com/public-yum-ol7.repo
+yum -y install yum-utils
+# yum-config-manager --enable ol7_UEKR4
+# yum-config-manager --disable ol7_UEKR3
+yum -y update
+
+# Set default Kernel to RHCK
+KERNEL=$(rpm -q kernel --queryformat "%{INSTALLTIME} %{VERSION}-%{RELEASE}\n" \
+          | sort -n | tail -n 1 | awk '{print $2}')
+MENUENTRY=$(grep -P "submenu|^menuentry" /boot/grub2/grub.cfg | cut -d "'" -f2 \
+          | grep $KERNEL)
+grub2-set-default "$MENUENTRY"
+
+# Install Cloud-Init
+yum install -y https://dl.fedoraproject.org/pub/epel/epel-release-latest-7.noarch.rpm
+yum-config-manager --enable ol7_optional_latest
+yum install -y cloud-init
+sed -i 's/name: fedora/name: ec2-user/g' /etc/cloud/cloud.cfg
+sed -i 's/gecos: Fedora Cloud User/gecos: EC2 Cloud User/g' /etc/cloud/cloud.cfg
+sed -i 's/distro: fedora/distro: rhel/g' /etc/cloud/cloud.cfg
+sed -i 's/lock_passwd: true/lock_passwd: false/g' /etc/cloud/cloud.cfg
+
+#
+cat /etc/oracle-release
+
+#
+sed -i 's/Defaults\ \ \ \ requiretty/Defaults   !requiretty/' /etc/sudoers
+sed -i 's/Defaults\ \ \ !visiblepw/Defaults    visiblepw/' /etc/sudoers
+
